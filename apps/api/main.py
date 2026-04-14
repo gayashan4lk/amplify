857f2b90a56737fd7e003e9cd6861ba16df72184
gayashan4lk <gayashan4lk@gmail.com>
2026-04-14 11:28:37 +0530

    create FastAPI app

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from api!")
+# def main():
+#     print("Hello from api!")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
